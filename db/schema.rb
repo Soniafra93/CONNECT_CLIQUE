e06bac1ed5f756d1a83358197e325025aaf1f51e
Hind NOUNI <nouni.hind@gmail.com>
2024-06-15 16:17:48 +0100

    added activity controller, view files, vote model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_15_121447) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_15_151446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,9 +65,21 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_15_121447) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.date "selected_date"
+    t.bigint "user_id", null: false
+    t.bigint "activity_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_votes_on_activity_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "activities", "users"
   add_foreign_key "attendances", "activities"
   add_foreign_key "attendances", "users"
   add_foreign_key "friends", "users"
   add_foreign_key "friends", "users", column: "attendee_id"
+  add_foreign_key "votes", "activities"
+  add_foreign_key "votes", "users"
 end
