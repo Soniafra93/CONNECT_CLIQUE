719b1245b921d0ef197755cb5bca721818d2f255
Laurent Sercomanens <aurovici@gmail.com>
2024-06-16 21:10:07 +0530

    add simple calendar gem, modify pages contoller, routes and pages view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
+    # Scope your query to the dates being shown:
+    start_date = params.fetch(:start_date, Date.today).to_date
+
+    # For a monthly view:
+    @meetings = Activity.where(date_1: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+  end
+
+  def index
+    # Scope your query to the dates being shown:
+    start_date = params.fetch(:start_date, Date.today).to_date
+
+    # For a monthly view:
+    @meetings = Activity.where(date_1: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 end
