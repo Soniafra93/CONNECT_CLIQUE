9bf1b90820bea9beb145d2e7552ff0d1c131aa1d
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-06-19 19:56:31 +0100

    added datepicker calendar to new activity

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,11 @@
+
+import flatpickr from "flatpickr";
+
+
+import "flatpickr/dist/flatpickr.min.css";
+
+document.addEventListener('DOMContentLoaded', () => {
+  flatpickr('.datepicker', {
+
+  });
+});
