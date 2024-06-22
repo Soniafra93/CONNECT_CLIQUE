ae62a8a0c061181dc05fa23f8d78bfc3228918cb
Hind NOUNI <nouni.hind@gmail.com>
2024-06-22 01:59:15 +0100

    added a better styling to the friends form, changed the logo

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,38 +1,43 @@
 class FriendsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_users, only: [:index, :create]
+  after_action :verify_authorized, except: [:index]
+  after_action :verify_policy_scoped, only: [:index]
 
   def index
-    @friends = current_user.friends
-    @friends = policy_scope(Friend)
-    @users = User.all.where.not(id: current_user.id)
+    @friends = policy_scope(Friend).includes(:attendee)
   end
 
   def create
-    @attendee = User.find(params[:attendee_id])
-
-    authorize(Friend)
-
-    if @attendee
-      if current_user.friends.exists?(attendee_id: @attendee.id)
-        redirect_to friends_path, notice: "Friend already added."
-      else
-        current_user.friends.create(attendee_id: @attendee.id)
+    @attendee = User.find_by(id: params[:friend][:attendee_id])
+    if @attendee && @attendee != current_user && !current_user.friends.exists?(attendee_id: @attendee.id)
+      friend = current_user.friends.new(attendee_id: @attendee.id)
+      authorize friend
+      if friend.save
         redirect_to friends_path, notice: "Friend added successfully."
+      else
+        redirect_to friends_path, alert: "Unable to add friend."
       end
     else
-      redirect_to friends_path, alert: "Unable to find friend with that name."
+      authorize current_user.friends.new
+      redirect_to friends_path, alert: "Unable to add friend."
     end
   end
 
   def destroy
-    friend = current_user.friends.find_by(id: params[:id])
-
-    if friend
-      authorize(friend)
-      friend.destroy
+    @friend = current_user.friends.find_by(id: params[:id])
+    authorize @friend
+    if @friend
+      @friend.destroy
       redirect_to friends_path, notice: "Friend removed successfully."
     else
       redirect_to friends_path, alert: "Unable to remove friend."
     end
+  end
+
+  private
+
+  def set_users
+    @users = User.where.not(id: current_user.id)
   end
 end
