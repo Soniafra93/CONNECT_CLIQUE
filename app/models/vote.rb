f1a3044d243832e7a6252a535454812005a9cf5b
Laurent Sercomanens <aurovici@gmail.com>
2024-06-26 22:32:31 +0530

    edit votes model to only allow one vote per person

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,5 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :activity
   validates :selected_date, presence: true
+  validates :user_id, uniqueness: { scope: :activity_id, message: "You can only vote once per activity" }
 end
